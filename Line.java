716a5bf49f00583ebe23b59b6f47628c72fe8e66
NikitaMasyutinIT13 <nikitamas58@gmail.com>
2026-05-12 19:56:46 +0500

    Update Line.java

--- Line.java
+++ Line.java
@@ -1,85 +1,89 @@
-
 public class Line {
-  private Point start;
-  private Point end;
-//FIXME: Код не должен содержать комментариев по типу "конструктор, геттер", не использовать сокращения
-  // // Конструктор
-  // public Line(Point start, Point end) {
-  //   this.start = start;
-  //   this.end = end;
-  // }
+    private Point start;
+    private Point end;
 
-  // // Конструктор для горизонтальной линии
-  // public Line(int y, int start_X, int end_X) {
-  //   this.start = new Point(start_X, y);
-  //   this.end = new Point(end_X, y);
-  // }
-//FIXTO:
-  public Line(Point start, Point end) {
-    this.start = start;
-    this.end = end;
-  }
-  //FIXME: не использовать сокращения
-  // public Line(int y, int x1, int x2) {
-  //   this.start = new Point(x1, y);
-  //   this.end = new Point(x2, y);
-  //FIXTO:
-   public Line(int y, int start_X, int end_X) {
-    this.start = new Point(start_X, y);
-    this.end = new Point(end_X, y);
-  }
-  //FIXME: Код не должен содержать комментариев по типу "конструктор, геттер", методы пишутся в PascalCase
+    //FIXME: Код не должен содержать комментариев по типу "конструктор, геттер", не использовать сокращения
+    // // Конструктор
+    // public Line(Point start, Point end) {
+    //   this.start = start;
+    //   this.end = end;
+    // }
 
-  // // Геттеры
-  // public Point getStart() {
-  //   return start;
-  // }
+    // // Конструктор для горизонтальной линии
+    // public Line(int y, int start_X, int end_X) {
+    //   this.start = new Point(start_X, y);
+    //   this.end = new Point(end_X, y);
+    // }
+    //FIXTO:
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
 
-  // public Point getEnd() {
-  //   return end;
-  // }
+    //FIXME: не использовать сокращения
+    // public Line(int y, int x1, int x2) {
+    //   this.start = new Point(x1, y);
+    //   this.end = new Point(x2, y);
+    // }
+    //FIXTO:
+    public Line(int y, int start_X, int end_X) {
+        this.start = new Point(start_X, y);
+        this.end = new Point(end_X, y);
+    }
 
-  // // Сеттеры
-  // public void setStart(Point start) {
-  //   this.start = start;
-  // }
+    //FIXME: Код не должен содержать комментариев по типу "конструктор, геттер", методы пишутся в PascalCase
 
-  // public void setEnd(Point end) {
-  //   this.end = end;
-  // }
-  //FIXTO:
-  public Point GetStart() {
-    return start;
-  }
+    // // Геттеры
+    // public Point getStart() {
+    //   return start;
+    // }
 
-  public Point GetEnd() {
-    return end;
-  }
+    // public Point getEnd() {
+    //   return end;
+    // }
 
-  public void SetStart(Point start) {
-    this.start = start;
-  }
+    // // Сеттеры
+    // public void setStart(Point start) {
+    //   this.start = start;
+    // }
 
-  public void SetEnd(Point end) {
-    this.end = end;
-  }
+    // public void setEnd(Point end) {
+    //   this.end = end;
+    // }
+    //FIXTO:
+    public Point GetStart() {
+        return start;
+    }
 
-  @Override
-  public String toString() {
-    return "Линия от " + start.toString() + " до " + end.toString();
-  }
-//FIXME: Код не должен содержать комментариев по типу "конструктор, геттер", методы пишутся в PascalCase, 
-//сокращения в названиях не допустимы
-  // // Метод для вычисления длины линии
-  // public double getLength() {
-  //   int dx = end.getX() - start.getX();
-  //   int dy = end.getY() - start.getY();
-  //   return Math.sqrt(dx * dx + dy * dy);
-  // }
-  //FIXTO:
-  public double GetLength() {
-    int delta_X = end.getX() - start.getX();
-    int delta_Y = end.getY() - start.getY();
-    return Math.sqrt(delta_X * delta_X + delta_Y * delta_Y);
-  }
+    public Point GetEnd() {
+        return end;
+    }
+
+    public void SetStart(Point start) {
+        this.start = start;
+    }
+
+    public void SetEnd(Point end) {
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "Линия от " + start.toString() + " до " + end.toString();
+    }
+
+    //FIXME: Код не должен содержать комментариев по типу "конструктор, геттер", методы пишутся в PascalCase, 
+    //сокращения в названиях не допустимы
+    // // Метод для вычисления длины линии
+    // public double getLength() {
+    //   int dx = end.getX() - start.getX();
+    //   int dy = end.getY() - start.getY();
+    //   return Math.sqrt(dx * dx + dy * dy);
+    // }
+    //FIXTO:
+    public double GetLength() {
+        int delta_X = end.getX() - start.getX();
+        int delta_Y = end.getY() - start.getY();
+        return Math.sqrt(delta_X * delta_X + delta_Y * delta_Y);
+    }
 }
